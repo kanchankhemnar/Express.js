40959c9747ef73d4ea7587bbb8f35596552ac203
kanchankhemnar <kanchankhemnar170@gmail.com>
2024-04-14 18:14:19 +0530

    a

---
+++ index.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 6;
+console.log(a,b);
+
+const express=require('express');
+const app=express();
+app.listen(8080);
+console.log("hello");
+console.log("hello");
+console.log("hello");
+
