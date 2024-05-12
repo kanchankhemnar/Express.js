07eaea43ea7b299e8f943f5239ce7fd631da7095
kanchankhemnar <kanchankhemnar170@gmail.com>
2024-05-12 22:35:39 +0530

    env added

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+require("dotenv").config();
+console.log("env", process.env.DB_PASSWORD);
 const express = require("express");
-
+const mongoose = require("mongoose");
 const server = express();
-
 const morgan = require("morgan");
-const router = require("./routes/productRoutes");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+  console.log("db connected");
+}
+
 // server.use(morgan("default"));
 
 server.use(express.static("public"));
 
 server.use(express.json());
+const router = require("./routes/productRoutes");
 
 server.use("/", router.router);
 
-server.listen(8080, () => {
+server.listen(process.env.PORT, () => {
   console.log("server start");
 });
